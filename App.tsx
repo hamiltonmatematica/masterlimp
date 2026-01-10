4503fad0fd224f1f08d77cd2735f0ff6c6908391
hamiltonmatematica <hamilton.vinicius@gmail.com>
2026-01-10 19:20:37 -0300

    Alterado fundo da seção Quem Somos para branco

--- App.tsx
+++ App.tsx
@@ -101,7 +101,7 @@ const App: React.FC = () => {
       </section>
 
       {/* About Section */}
-      <section id="sobre" className="py-24 bg-sky-cyan overflow-hidden">
+      <section id="sobre" className="py-24 bg-white overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <div className="order-2 lg:order-1">

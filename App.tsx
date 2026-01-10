6286524c44ed03db245b64d099eddd1520779493
hamiltonmatematica <hamilton.vinicius@gmail.com>
2026-01-10 11:35:38 -0300

    Background da seção Sobre alterado para azul ciano #87CEEB

--- App.tsx
+++ App.tsx
@@ -111,7 +111,7 @@ const App: React.FC = () => {
       </section>
 
       {/* About Section */}
-      <section id="sobre" className="py-24 bg-blue-50 overflow-hidden">
+      <section id="sobre" className="py-24 bg-sky-cyan overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <div className="order-2 lg:order-1">
